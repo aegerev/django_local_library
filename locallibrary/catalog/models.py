f4f6686037d05122c13f508cdd96962b5268fcfb
Eric D. Triton <alexandersegerev@gmail.com>
2025-05-21 14:15:58 -0400

    Added everything related to the administrator; modified the Book model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -56,6 +56,10 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
     
+    def display_genre(self):
+         return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'
+    
     '''The BookInstance Model'''
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="I need to have the book's Unique ID so that I could search the whole library for it. ")
